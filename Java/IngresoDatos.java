fb432aeebd0b35b678286305aebb76e8c9c103ac
ThiagoMirandaLiotto <thiagomirandaliotto99@gmail.com>
2025-03-31 12:42:51 -0300

    Ejercicios IngresoDatos y NumeroMayor de java hechos

---
+++ Java/IngresoDatos.java
@@ -0,0 +1,23 @@
+public class IngresoDatos {
+    public static void main(String[] args) {
+        //Declarar y dar valores a las variables
+        int N = 32;
+        double A = 54;
+        char C = 'L';
+
+        //Mostrar las variables
+        System.out.println(N);
+        System.out.println(A);
+        System.out.println(C);
+
+        //'N + A' - 'A - N' - 'Valor numerico de A'
+        System.out.println(N + A);
+        System.out.println(A - N);
+
+        int intC = C;
+        System.out.println(intC);
+
+
+
+    }
+}
---
+++ Java/NumeroMayor.java
@@ -0,0 +1,13 @@
+public class NumeroMayor {
+    public static void main(String[] args) {
+        int A = 67  ;
+        int B = 65;
+
+        if (A > B){
+            System.out.println("A es la variable mayor");
+        }else{
+            System.out.println("B es la variable mayor");
+        }
+
+    }
+}
