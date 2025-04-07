94b80e6de89e994a71d1f8e034e649de14b8c535
GARYPOU <tomasgarayzar@gmail.com>
2025-04-07 12:38:41 -0300

    Agrego algunos ej de java

---
+++ Java/EJ3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EJ3 {
+
+    public static void main(String[] args) {
+    int N = 0;
+    Scanner e = new Scanner(System.in);
+    System.out.println("Todos los numeros pares hasta el 100: ");
+    for (int i = 0; i < 50; i++)
+        N = N + 2;
+        System.out.println(N);
+        System.out.println( );
+    }
+}
+
---
+++ Java/Vocales.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Vocales {
+    public static void main(String[] args) {
+        String texto = "Ayer me compre muñecos de la marca 'ToyCo' por internet.";
+        int contadorvocales = 0;
+            for (int i = 0; i < texto.length(); i++) {
+                char c = texto.charAt(i);
+
+                if (c == 'a' || c == 'A' || c == 'á' || c == 'Á' || c == 'e' || c == 'E' || c == 'é' || c == 'É' || c == 'i' || c == 'I' || c == 'í' ||
+                        c == 'Í' ||c == 'o' || c == 'O' || c == 'ó' || c == 'Ó' ||c == 'u' || c == 'U' || c == 'ú' || c == 'Ú' )
+                {
+                contadorvocales++;
+                }
+            }
+        System.out.println(contadorvocales);
+    }
+}
