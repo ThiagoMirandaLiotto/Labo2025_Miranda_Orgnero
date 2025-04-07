90d4641592224db3ac4e4b8d9e7dd56c4628dd0a
GARYPOU <tomasgarayzar@gmail.com>
2025-04-07 09:37:22 -0300

    prueba1

---
+++ Java/ingresodatos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ingresodatos {
+
+    public static void main(String[] args) {
+    int N;
+    double A;
+    char C;
+    
+    Scanner e = new Scanner(System.in);
+        System.out.println("Ingresar un entero: ");
+        N = e.nextInt();
+        System.out.println("Ingresar un double: ");
+        A = e.nextDouble();
+        System.out.println("Ingresar un char: ");
+        C = (e.nextLine()).charAt(0);
+
+        System.out.println(N);
+        System.out.println(A);
+        System.out.println(C);
+
+        System.out.println(N+A);
+        System.out.println(A-N);
+    }
+}
+
