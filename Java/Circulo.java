7216e9334786ac34c4ab5235dec146764ec975be
ThiagoMirandaLiotto <thiagomirandaliotto99@gmail.com>
2025-04-09 09:31:11 -0300

    Terminados ejercicios clase pasada, iniciado con lo de objetos

---
+++ Java/Circulo.java
@@ -0,0 +1,27 @@
+public class Circulo {
+    private float radio;
+
+    public Circulo(){
+        this.radio = 2;
+    }
+    public Circulo(float radio){
+        this.radio = radio;
+    }
+
+    public float getRadio(){
+        return radio;
+    }
+    public void setRadio(float radio){
+        this.radio = radio;
+    }
+
+    public float calcularArea(){
+        float area = (float) ((radio*radio) * 3.14);
+        return area;
+    }
+
+    public float calcularPerimetro(){
+        float perimetro = (float) (2 * radio * 3.14);
+        return perimetro;
+    }
+}
---
+++ Java/ReemplazarLetra.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReemplazarLetra {
+    public static void main(String[] args) {
+        String frase = "Ayer me compré muñecos de la marca ‘ToyCo’ por internet.";
+
+        System.out.println("Ingrese una letra");
+        Scanner e = new Scanner(System.in);
+        String letra = e.nextLine();
+
+        String frase2 = frase.replace('e', letra.charAt(0));
+
+        System.out.println(frase2);
+
+    }
+}
