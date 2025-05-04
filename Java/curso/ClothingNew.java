13ee4ea664f5133179968c2328f9d162a4636eea
ThiagoMirandaLiotto <168751488+ThiagoMirandaLiotto@users.noreply.github.com>
2025-05-04 19:55:55 -0300

    Add files via upload

---
+++ Java/curso/ClothingNew.java
@@ -0,0 +1,55 @@
+package objetos;
+
+public class Clothing
+
+{
+
+        private String description;
+        private double price;
+        private char size='s';
+        private final double min_tax=0.2;
+        private final int min_price=10;
+
+        public Clothing(String description, double price, char size){
+                this.description=description;
+                this.price=price;
+                this.size=size;
+
+        }
+
+        public String getDescription() {
+                return description;
+        }
+
+        public void setDescription(String description) {
+                this.description = description;
+        }
+
+        public double getPrice() {
+                price = price + (price * min_tax);
+                return price;
+        }
+
+        public void setPrice(double price) {
+                if(price<min_price) {
+                        this.price = min_price;
+                }
+                this.price = price;
+        }
+
+        public char getSize() {
+                return size;
+        }
+
+        public void setSize(char size) {
+                this.size = size;
+        }
+
+        public static void main(String[] args) {
+
+          
+
+
+          
+        }
+    }
